bc6c9777986dd6588a6febd891027a890435a01e
CL Fisher <clfisher@rogers.com>
2022-10-16 13:26:14 -0400

    changed client share to integer.  added addkeyup as trigger for calculating the total

--- app/assets/javascripts/partner_county.js
+++ app/assets/javascripts/partner_county.js
@@ -1,7 +1,18 @@
 function calculate_client_share_total(){
     total = 0;
     const client_shares = document.querySelectorAll('*[id$="client_share"]')
-    client_shares.forEach(cs => total += parseInt(cs.value));
+    client_shares.forEach(
+        cs => {
+            if (cs.value) {
+                total += parseInt(cs.value)
+            }
+        }
+        );
     document.getElementById("partner_county_client_share_total").innerHTML =total;
+    if(total == 0 || total == 100){
+        document.getElementById("partner_county_client_share_total_warning").style.visibility= 'hidden';
+    }else {
+        document.getElementById("partner_county_client_share_total_warning").style.visibility= 'visible';
+    }
     return total;
 }
